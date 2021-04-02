05e83f40e17197f0bbc271767cc6807bc6a51e2b
reena-50 <67496021+reena-50@users.noreply.github.com>
2021-04-02 11:38:51 +0530

    check env var

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        {process.env.REACT_APP_NAME}
         </a>
       </header>
     </div>
